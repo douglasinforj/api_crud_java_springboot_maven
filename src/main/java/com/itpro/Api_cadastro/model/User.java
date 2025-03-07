afab750fbd4d2210351f6567fceec6abcb115329
Douglas Silva <douglasitpro@gmail.com>
2025-03-07 17:05:21 -0300

    Adicionando Class para armazenamento de Users

---
+++ src/main/java/com/itpro/Api_cadastro/model/User.java
@@ -0,0 +1,25 @@
+package com.itpro.Api_cadastro.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
